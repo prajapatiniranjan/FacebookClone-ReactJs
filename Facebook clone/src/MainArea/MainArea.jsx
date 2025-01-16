2372c487ec56a69defb3d9ca3f7f5500cad66efb
Niranjan Prajapati <81921820+prajapatiniranjan@users.noreply.github.com>
2025-01-17 02:23:15 +0530

    React Js face book work in progress

---
+++ Facebook clone/src/MainArea/MainArea.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import './MainArea.css';
+import { FaFontAwesomeFlag, FaMedrt, FaFacebookMessenger, FaAngleDown, FaUserFriends } from 'react-icons/fa';
+
+function MainArea() {
+  return (
+    <div className="Main">
+      <div className="Rside">
+        <div className="Profile">
+          <img src="" alt="Dp" style={{ height: "30px", padding: "1rem" }} />
+          Mr.Max
+        </div>
+        <div className="Pro">
+          <FaFontAwesomeFlag color="green" fontSize="2rem" />
+          <div>Pages</div>
+        </div>
+        <div className="Pro">
+          <FaMedrt color="blue" fontSize="2rem" />
+          <div>FSD Java Information</div>
+        </div>
+        <div className="Pro">
+          <FaFacebookMessenger color="green" fontSize="2rem" />
+          <div>Messenger for Kids</div>
+        </div>
+        <div className="Pro">
+          <FaUserFriends color="blue" fontSize="2rem" />
+          <div>Friends</div>
+        </div>
+        <div className="Pro">
+          <FaAngleDown color="green" fontSize="2rem" />
+          <div>See More</div>
+        </div>
+      </div>
+
+      <div className="MainArea">
+        Middle
+      </div>
+
+      <div className="Lside">
+        Lside
+      </div>
+    </div>
+  );
+}
+
+export default MainArea;
